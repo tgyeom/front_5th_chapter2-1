5f5388eb0532ea82fbbcd6faef843938d66b5715
tgyeom <ktg5679@gmail.com>
2025-04-18 20:05:48 +0900

    🚀배포를 위한 세팅

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,19 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/front_5th_chapter2-1/',
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: 'src/setupTests.js'
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        basic: './index.basic.html',
+        advanced: './index.advanced.html',
+      },
+    },
   },
 })
